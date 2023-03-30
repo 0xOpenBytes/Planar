8ec165bda147fb51cfd1ead0faadf328030ce88f
Leif <zmeriksen@icloud.com>
2023-03-30 17:13:30 -0600

    Update Scribe

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .package(url: "https://github.com/0xOpenBytes/Cache.git", from: "0.1.0"),
         .package(url: "https://github.com/0xOpenBytes/TaskManager", from: "0.1.0"),
         .package(url: "https://github.com/0xOpenBytes/Disk", from: "0.1.0"),
-        .package(url: "https://github.com/0xLeif/Scribe", from: "1.0.0")
+        .package(url: "https://github.com/0xLeif/Scribe", from: "2.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
